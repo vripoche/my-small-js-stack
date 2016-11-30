9ab31e52f43999a2e4a3d63977fb20ead72f692b
Vivien Ripoche <vivien.ripoche.ext@mappy.com>
2016-11-30 10:59:39 +0100

    No imports in tests bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ gulp.task('connect', function() {
 });
 
 gulp.task('test', function() {
-  mochify('./test/bootstrap.js', {
+  mochify('./test/**/*.js', {
     reporter : 'spec',
     cover    : false,
     watch    : true
